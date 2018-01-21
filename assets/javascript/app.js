8a1245820a1798f9520c3327aa3efbf909d3c8e9
FuzzyJones <donald.jones83@gmail.com>
2018-01-21 15:33:13 -0500

    more styling

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,12 +3,12 @@
 // https://api.giphy.com/v1/gifs/search?api_key=xFHtqYPE42jbIneYyI0aPoygDEg0EZwA&q=&limit=25&offset=0&rating=PG&lang=en
 
 
-var characters = ["Jon Snow", "The Mountain", "Tyrion Lannister", "Bran Stark", "Cersei Lannister", "Ice King", "Hodor", "Arya Stark", "Melisandre"]
+var characters = ["Jon Snow", "The Mountain", "Tyrion Lannister", "Bran Stark", "Cersei Lannister", "Ice King", "Hodor", "Arya Stark"]
 
 function displayCharacterInfo() {
     
-    var characters = $(this).attr("data-name");
-    var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=" + characters + "xFHtqYPE42jbIneYyI0aPoygDEg0EZwA&q=&limit=25&offset=0&rating=PG&lang=en";
+    var characters = $(this).attr();
+    var queryURL = "https://api.giphy.com/v1/gifs/search?=" + characters + "xFHtqYPE42jbIneYyI0aPoygDEg0EZwA&q=&limit=10&offset=0&rating=G&lang=en";
 
     $.ajax({
         url: queryURL,
