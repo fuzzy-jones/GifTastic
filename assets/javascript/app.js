32c130adf80bf6073e768fe5788f2decc886a57c
FuzzyJones <donald.jones83@gmail.com>
2018-01-26 19:11:44 -0500

    complete

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -70,13 +70,23 @@ function displayCharacterInfo() {
 
         // creating a for loop to select the first 10 giphys of data returned from api
         for (i = 0; i < 10; i++) {
-            // getting the gif url and storing it in a new variable
+
+            // targeting the gif url in the object and storing it in a new variable
             var gifURL = response.data[i].images.fixed_height.url
             // creating an image div with the attribute source of the gifUrl variable and storing it in a new variable 
-            var gif = $("<img>").attr("src", gifURL)
+            var gif = $("<img>").attr("src", gifURL);
+            // add a class of gif to the gif variable to make it clickable by class play/pause onclick function
+            gif.addClass("gif");
+            // add attributes of "data-still" to gif and the still image response of gif to use in play pause onclick
+            gif.attr("data-still", response.data[i].images.fixed_height_still.url);
+            // add attribute of "data-animate" to gif and the regular playing response from gifUrl to later reanimate the gif in play pause function
+            gif.attr("data-animate", response.data[i].images.fixed_height.url);
+            // gif the gif variable a "data-state" attribute of animate to so we can have it playing by default in play/pause function
+            gif.attr("date-state", "animate");
             // appending the gif variable with a stored value of the url address in the character div variable 
             characterDiv.append(gif);
-            // creating  variable of rating to store the rating of each gif targeted
+
+            // targeting the rating url in the object and storing it in a rating variable
             var rating = response.data[i].rating;
             // creating variable to store the html text and stored rating
             var rated = $("<p>").text("Rated: " + rating);
@@ -87,6 +97,21 @@ function displayCharacterInfo() {
         // displaying the stored rating and url address from the characterDiv in the html with an id of giphy-view
         $("#giphy-space").html(characterDiv);
 
+    });
+
+    // pause and play gif
+    $(document).on("click", ".gif", function() {
+        // create a variable and storing the value of the attribute "data-state" in it
+        var state = $(this).attr("data-state");
+        // when the gif is clicked, if the data-state is equal to "animate" the gif will set the "data-still" stored url and pause, and change the "data-state" to "still"
+        // else if the "data-state" is "still" which it was changed to when clicked, when it is clicked again it will return to "data-animate" and play gif, and set the "data-state" back to "animate"
+        if (state === "animate") {
+          $(this).attr("src", $(this).attr("data-still"));
+          $(this).attr("data-state", "still");
+        } else {
+          $(this).attr("src", $(this).attr("data-animate"));
+          $(this).attr("data-state", "animate");
+        }
     });
 
 }
